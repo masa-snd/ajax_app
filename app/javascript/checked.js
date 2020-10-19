25e6abeefa52a4a002083b3afc1695aa91e0816f
masa-snd <71821815+masa-snd@users.noreply.github.com>
2020-10-19 16:43:53 +0900

    既読機能のフロントエンド実装

---
+++ app/javascript/checked.js
@@ -0,0 +1,31 @@
+function check() {
+  const posts = document.querySelectorAll(".post");
+  posts.forEach(function (post) {
+    if (post.getAttribute("data-load") != null) {
+      return null;
+    }
+    post.setAttribute("data-load", "true");
+    post.addEventListener("click", () => {
+      const postId = post.getAttribute("data-id");
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/posts/${postId}`, true);
+      XHR.responseType = "json";
+      XHR.send();
+      // ここまでの記述で投稿したデータをclickするイベントを発火させてそのデータを非同期通信でコントローラーへ情報を送るという意味の記述をしている
+      XHR.onload = () => {
+        if (XHR.status != 200) {
+          alert(`Error ${XHR.status}: ${XHR.statusText}`);
+          return null;          
+        }
+        const item = XHR.response.post;
+        if (item.checked === true) {
+          post.setAttribute("data-check", "true");
+        } else if (item.checked === false) {
+          post.removeAttribute("data-check");
+        }
+      };
+      // この処理でレスポンスがあった場合の処理を記述している
+    });
+  });
+}
+setInterval(check, 1000);
